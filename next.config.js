9c83820afc85833d8cd8b95e0b294786366f023a
zxchub <edik.nabok@gmail.com>
2022-07-28 00:11:25 +0300

    init commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['image.winudf.com','lh3.googleusercontent.com' ],
+  }
 }
 
 module.exports = nextConfig
